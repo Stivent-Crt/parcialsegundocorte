af777ceb3de7b1fbe6164f466174e5f65b70ae3b
Jaal-09 <jaal9709+github@gmail.com>
2026-05-20 11:43:31 -0500

    Implementando los metodos en la clase Libro

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -15,4 +15,20 @@ Libro::Libro(std::string titulo, std::string autor, std::string codigoLibro){
             break;
         }
     }
+
+    if(soloNumeros && !codigoLibro.empty()){
+        this->codigoLibro = codigoLibro;
+    } else {
+        this->codigoLibro = "000000";
+        std::cout < "Error solo debe contener números.\n";
+    }
+
+    this->disponible = true;
+
+    void Libro::mostrarInformacion(){
+        std::cout << "Titulo: " << titulo << std::endl;
+        std::cout << "Autor: " << autor << std::endl;
+        std::cout << "Código: " << codigoLibro << std::endl;
+        std::cout << "Estado: " << (disponible ? "Disponible": "Prestado") << std::endl;
+    }
 }
