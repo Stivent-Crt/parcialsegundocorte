e17b553209a94e3d6be5681c96cb62ad158e6afd
Jaal-09 <jaal9709+github@gmail.com>
2026-05-20 10:55:44 -0500

    Empezando la creacion de la cabecera de la clase Libro

---
+++ include/Libros.h
@@ -0,0 +1,18 @@
+#ifndef LIBRO_H
+#define LIBRO_H
+
+#include <iostream>
+#include <string>
+
+
+class Libro{
+    private:
+        std::string titulo;
+        std::string autor;
+        int codigoLibro;
+        bool disponible;
+    
+    public:
+        
+
+}
