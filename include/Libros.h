efb9551e7ff09e458698c8c5a469e74bc5b27ef8
Jaal-09 <jaal9709+github@gmail.com>
2026-05-20 11:19:21 -0500

    Terminada la creacion de la cabecera de Libro

--- include/Libros.h
+++ include/Libros.h
@@ -32,11 +32,50 @@ class Libro{
         Libro(std::string titulo, std::string autor, int codigoLibro, bool disponible);
 
 
+        
+        //Getters
+        std::string getTitulo() const;
+
+        std::string getAutor() const;
+
+        int getCodigoLibro() const;
+
+        bool getDisponible() const;
+
+        //Setters
+        void setTitulo(std::string titulo);
+
+        void setAutor(std::string autor);
+
+        void setCodigoLibro(int codigoLibro);
+
+        void setDisponible(bool disponible);
+
+        //Metodos
+
+        /**
+         * @brief Muestra la informacion del libro
+         * 
+         */
         void mostrarInformacion();
 
+        /**
+         * @brief Presta el libro si esta disponible
+         * @return retorna verdadero si lo esta de lo constrario falso
+         */
         void prestarLibro();
 
+        /**
+         * @brief Devuelve el libro
+         * 
+         */
         void devolverLibro();
 
+        /**
+         * @brief Getters y Setters de los atributos
+         * 
+         */
 
-}
+};
+
+#endif
