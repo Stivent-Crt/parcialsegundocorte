ce7a32244013b536ee038406f56a4bf50d59d424
Jaal-09 <jaal9709+github@gmail.com>
2026-05-20 14:38:47 -0500

    Terminada la cabecera de la clase Usuario

--- include/Usuario.h
+++ include/Usuario.h
@@ -10,3 +10,31 @@ private:
 
 public:
     Usuario(std::string nombre, std::string documento, int edad, std::string codigoUsuario);
+
+    // Setters
+    void setCodigoUsuario(std::string nuevoCodigo) {
+        bool soloNumeros = true;
+        for (char c : nuevoCodigo) {
+            if (!isdigit(c)) {
+                soloNumeros = false;
+                break;
+            }
+        }
+        if (soloNumeros && !nuevoCodigo.empty()) {
+            codigoUsuario = nuevoCodigo;
+        } else {
+            std::cout << "Error: El código debe contener solo números\n";
+        }
+    }
+    void setCantidadLibrosPrestados(int cantidad) { 
+        cantidadLibrosPrestados = (cantidad >= 0) ? cantidad : 0; 
+    }
+    
+    // Métodos para modificar la cantidad
+    void incrementarLibrosPrestados() { cantidadLibrosPrestados++; }
+    void decrementarLibrosPrestados() { 
+        if (cantidadLibrosPrestados > 0) cantidadLibrosPrestados--;
+    }
+};
+
+#endif
