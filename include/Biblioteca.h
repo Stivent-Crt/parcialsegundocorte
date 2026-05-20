031b73a5ac99c0529f3c32033fb31032494be6f5
Jaal-09 <jaal9709+github@gmail.com>
2026-05-20 15:13:54 -0500

    Agregadas las declaraciones de los metodos,Terminada la cabecera del menu

--- include/Biblioteca.h
+++ include/Biblioteca.h
@@ -8,5 +8,30 @@
 #include "Libro.h"
 
 class Biblioteca {
+    private:
+        std::vector<Usuario*> usuarios;
+        std::vector<Bibliotecario*> bibliotecarios;
+        std::vector<Libro*> libros;
+    
+        bool soloNumeros(std::string str);
+        Usuario* buscarUsuario(std::string codigo);
+        Libro* buscarLibro(std::string codigo);
 
+
+    public:
+
+        //Declaración de metodos
+        Biblioteca();
+        ~Biblioteca();
+    
+        void registrarUsuario();
+        void registrarBibliotecario();
+        void registrarLibro();
+        void prestarLibro();
+        void devolverLibro();
+        void mostrarUsuarios();
+        void mostrarLibros();
+};
+
+#endif
 };
