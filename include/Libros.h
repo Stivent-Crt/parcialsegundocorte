8690300150201c9e26986d6dca8dc036415d5c2e
Jaal-09 <jaal9709+github@gmail.com>
2026-05-20 11:07:57 -0500

    constructor agregado a la clase Libro de la cabecera

--- include/Libros.h
+++ include/Libros.h
@@ -4,6 +4,13 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief 
+ * @class Libro, representa un libro en la biblioteca
+ * @param string titulo, autor
+ * @param int codigoLibro
+ * @param bool ddisponible
+ */
 
 class Libro{
     private:
@@ -13,6 +20,23 @@ class Libro{
         bool disponible;
     
     public:
-        
+
+        /**
+         * @brief Constructor de libro
+         * 
+         * @param titulo 
+         * @param autor 
+         * @param codigoLibro 
+         * @param disponible 
+         */
+        Libro(std::string titulo, std::string autor, int codigoLibro, bool disponible);
+
+
+        void mostrarInformacion();
+
+        void prestarLibro();
+
+        void devolverLibro();
+
 
 }
