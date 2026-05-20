204298b04f30503ae04a9f6aaa6c5498ba087878
Jaal-09 <jaal9709+github@gmail.com>
2026-05-20 14:46:15 -0500

    Verificaciones agregadas en el constructor de Usuario

--- src/Usuario.cpp
+++ src/Usuario.cpp
@@ -3,5 +3,23 @@
 
 Usuario::Usuario(std::string nombre, std::string documento, int edad, std::string codigoUsuario)
     : Persona(nombre, documento, edad){
-        
+    
+    // Validación de código
+    bool soloNumeros = true;
+    for (char c : codigoUsuario) {
+        if (!isdigit(c)) {
+            soloNumeros = false;
+            break;
+        }
     }
+
+    if (soloNumeros && !codigoUsuario.empty()) {
+        this->codigoUsuario = codigoUsuario;
+    } else {
+        this->codigoUsuario = "000000";
+        std::cout << "Error: El código debe contener solo números. Se asignó 000000.\n";
+    }
+    
+    this->cantidadLibrosPrestados = 0;
+
+}
